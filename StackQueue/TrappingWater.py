e799881c80588a4b19106263edcf2531ce92cc21
limaries30 <limaries30@gmail.com>
2020-08-01 16:35:29 +0900

    brute_force

--- StackQueue/TrappingWater.py
+++ StackQueue/TrappingWater.py
@@ -1,4 +1,18 @@
 class Solution:
+    '''brute force'''
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right= len(height)-1
+        answer = 0
+        for idx,wall in enumerate(height):
+            left_max = max(height[:idx+1])
+            right_max = max(height[idx:])
+            min_wall = min(left_max,right_max)
+            answer += max(min_wall-wall,0)
+        return answer
+
+
+class Solution:
     '''two pointer solution'''
     def trap(self, height: List[int]) -> int:
         left = 0
