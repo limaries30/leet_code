a10b582614d7564d20f28be1c2f31b3804bea348
limaries30 <limaries30@gmail.com>
2020-10-14 21:28:58 +0900

    RM .vscode

--- LinkedList/LRUcache.py
+++ LinkedList/LRUcache.py
@@ -18,8 +18,8 @@ class DLL:
 
     def remove(self, node):
         node.prev.next = node.next
-        if node.next:
-            node.next.prev = node.prev
+        node.next.prev = node.prev
+
         self.size -= 1
 
     def unshift(self, node):
@@ -27,6 +27,13 @@ class DLL:
         self.head.next.prev = node
         self.head.next = node
         node.prev = self.head
+        # if self.head.next:
+        #     self.head.next.prev = node
+        #     node.next = self.head.next
+        # else:
+        #     self.tail = node
+        # node.prev = self.head
+        # self.head.next = node
         self.size += 1
 
 
@@ -49,46 +56,16 @@ class LRUCache:
 
         if key in self.hashmap.keys():
             node = self.hashmap[key]
+            node.value = value
             self.dll.remove(node)
             self.dll.unshift(node)
         else:
             node = Node(value, key)
             if self.dll.size == self.dll.max_size:
                 self.hashmap.pop(self.dll.tail.prev.key)
-                self.dll.remove(self.dll.tail)
+                self.dll.remove(self.dll.tail.prev)
                 self.dll.unshift(node)
                 self.hashmap[key] = node
             else:
                 self.hashmap[key] = node
                 self.dll.unshift(node)
-
-
-# orders = ["LRUCache", "put", "get", "put", "get", "get"]
-# values = [[1], [2, 1], [2], [3, 2], [2], [3]]
-
-orders = [
-    "LRUCache",
-    "put",
-    "put",
-    "put",
-    "put",
-    "get",
-    "get",
-    "get",
-    "get",
-    "put",
-    "get",
-    "get",
-    "get",
-    "get",
-    "get",
-]
-values = [[3], [1, 1], [2, 2], [3, 3], [4, 4], [4], [3], [2], [1], [5, 5], [1], [2], [3], [4], [5]]
-
-for idx, order in enumerate(orders):
-    if idx == 0:
-        LRU = LRUCache(values[0][0])
-    if order == "put":
-        LRU.put(values[idx][0], values[idx][1])
-    if order == "get":
-        LRU.get(values[idx][0])
