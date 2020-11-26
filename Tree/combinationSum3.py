dde6a2a661e1b3a8878e8dfe7fceed0944fade75
limaries30 <limaries30@gmail.com>
2020-11-27 00:48:20 +0900

    combination sum3

---
+++ Tree/combinationSum3.py
@@ -0,0 +1,26 @@
+'''
+https://leetcode.com/problems/combination-sum-iii/
+'''
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        
+        self.k = k
+        self.n = n
+        self.answer = []
+
+        self.dfs([],1)
+        
+        return self.answer
+        
+    
+    def dfs(self,res,current):
+        
+        if len(res)>=self.k or current>9:
+            if sum(res)==self.n and len(res)==self.k:
+                self.answer.append(res.copy())
+            return
+        
+        res.append(current)
+        self.dfs(res,current+1)
+        res.pop()
+        self.dfs(res,current+1)
