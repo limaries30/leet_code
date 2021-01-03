bc31ad78f9c024afc4b6bb381dbdb1e1614c7c54
limaries30 <limaries30@gmail.com>
2021-01-03 23:27:10 +0900

    ADD k-concatenation

--- DP/K-Concatenation Maximum Sum.py
+++ DP/K-Concatenation Maximum Sum.py
@@ -1,7 +1,33 @@
 "https://leetcode.com/problems/k-concatenation-maximum-sum/"
 
+class Solution:
+    def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
+        
+        sum_arr = sum(arr)
+        max_sum_arr = self.maxSubarray(arr)
+        max_sum_arr_two = self.maxSubarray(arr*2)
+        
+        MODULO = 10**9 + 7
+        
+        if k==1:
+            return max_sum_arr%MODULO
+        if sum_arr<0:
+            return max(max_sum_arr,max_sum_arr_two)%MODULO
+        else:
+            return (max_sum_arr_two+(k-2)*sum_arr)%MODULO
+        
+    def maxSubarray(self,arr):
+        local_max = -float('inf')
+        global_max = -float('inf')
+        for i in arr:
+            local_max = max(local_max+i,i)
+            global_max = max(local_max,global_max)
+        return max(global_max,0)
+
+
 
 class Solution:
+    '''same solution(unreadable)'''
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
 
         MODULO = 10 ** 9 + 7
