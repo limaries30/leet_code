be5c47394513065300fdbe5e5ee61eec80649ce1
limaries30 <limaries30@gmail.com>
2021-02-12 20:04:54 +0900

    pizza

--- DP/Pizza With 3n Slices.py
+++ DP/Pizza With 3n Slices.py
@@ -1,20 +1,24 @@
-'''
+"""
 https://leetcode.com/problems/pizza-with-3n-slices/
-'''
+"""
+
+
 class Solution:
     def maxSizeSlices(self, slices: List[int]) -> int:
-        
-        total_pieces = len(slices)
-        self.my_pieces = len(slices)//3
-        
-        return max(self.dp_build(slices[1:]),self.dp_build(slices[:-1]))
-    
-    def dp_build(self,arr):
-        
-        pieces = len(arr)
-        dp = [[0]*pieces for i in range(pieces)]
-        for i in range(pieces):
-            for j in range(self.my_pieces):
-                dp[i][j]=max(dp[i-1][j],dp[i-2][j-1]+arr[i])
-                
-        return dp[pieces-1][self.my_pieces-1]
+
+        start_end_sum = slices[0] + slices[-1]
+        quota = len(slices) // 3
+
+        return max(self.dp(slices[:-1], quota), self.dp(slices[1:], quota))
+
+    def dp(self, slices, quota):
+
+        arr = [[0] * (quota + 1) for i in range(len(slices) + 1)]
+        # arr[0][0]=slices[0]
+        for i in range(1, len(slices) + 1):
+            for j in range(1, quota + 1):
+                if i == 1:
+                    arr[i][j] = slices[i - 1]
+                    continue
+                arr[i][j] = max(arr[i - 2][j - 1] + slices[i - 1], arr[i - 1][j])
+        return arr[-1][-1]
